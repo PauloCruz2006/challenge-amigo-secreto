3837f121c17704248a7543731b6dc0c71aaefd75
PauloCruz2006 <paulocruz.02006@gmail.com>
2025-03-12 13:06:35 -0300

    Exclusão do readm-script.js

--- readme-script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var vercelLink = document.getElementById('vercel-link');
-    if (vercelLink) {
-        vercelLink.setAttribute('target', '_blank');
-    }
-});
